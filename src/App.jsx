5b9c10757e006735e972fde9b5a16d628fedee10
taufan.zamzami <taufan.zamzami@jamiiis.local>
2024-11-08 23:57:31 +0700

    restart everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import MainPage from './components/MainPage';
 import NavBar from './components/NavBar';
-import Contact from './components/Contact';
+import Contact from './components/Contact'; 
 
 function App() {
   return (
