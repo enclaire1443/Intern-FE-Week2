8465a4f98317dde458a412955e304cf4c9811646
taufan.zamzami <taufan.zamzami@jamiiis.local>
2024-11-08 22:43:16 +0700

    Update to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import MainPage from './components/MainPage';
 import NavBar from './components/NavBar';
-import Contact from './components/Contact'; 
+import Contact from './components/Contact';
 
 function App() {
   return (
